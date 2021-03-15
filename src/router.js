bdef1f77aece92d07a7c88ece0f7ebf7adb8a780
Jhon Banguera <jhon.abanguera@gmail.com>
2021-03-15 04:45:55 -0600

    Dinamic routes

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "@/views/Home";
 import About from "@/views/About";
 import Error from "@/views/Error";
+import CoinDetail from '@/views/CoinDetail'
 
 //Función que nos permite instalar plugings o incorporar diferentes plugins
 //o utilidades que tiene la libreria
@@ -23,6 +24,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: '/coin/:id',
+      name: 'coin-detail',
+      component: CoinDetail
     },
     {
       path: "*",
